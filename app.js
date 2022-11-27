e92ff3d213e6968809e846d4201350485f730b3c
Victor Mendoza <72841783+iVictoremv@users.noreply.github.com>
2022-11-26 21:43:06 -0600

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const validateForm = () => {
-    let x = document.forms["mainContainer"]["fname"].value;
-    if (x == "") {
-      alert("Ingrese un dato en el campo");
-      return false;
-    }
-  }
